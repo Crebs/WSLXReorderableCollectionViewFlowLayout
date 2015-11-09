44d6dfe2550629a0e33dd4c930155bf469ba9a84
Riley Crebs <rcrebs@salesforce.com>
2015-11-09 12:20:20 -0800

    carthage support

---
+++ WSLXReorderableCollectionViewFlowLayout/WSLXReorderableCollectionViewFlowLayout.h
@@ -0,0 +1,21 @@
+//
+//  WSLXReorderableCollectionViewFlowLayout.h
+//  WSLXReorderableCollectionViewFlowLayout
+//
+//  Created by Riley Crebs on 11/9/15.
+//  Copyright © 2015 Incravo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WSLXReorderableCollectionViewFlowLayout.
+FOUNDATION_EXPORT double WSLXReorderableCollectionViewFlowLayoutVersionNumber;
+
+//! Project version string for WSLXReorderableCollectionViewFlowLayout.
+FOUNDATION_EXPORT const unsigned char WSLXReorderableCollectionViewFlowLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WSLXReorderableCollectionViewFlowLayout/PublicHeader.h>
+
+#import <WSLXReorderableCollectionViewFlowLayout/LXReorderableCollectionViewFlowLayout.h>
+
+
